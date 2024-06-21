bb863c2d3f0a62c7ba9b76f84fb49a73a4e9bdb5
rachit <rachitgarg1208@gmail.com>
2024-06-21 19:27:32 +0530

    Music App Completed

--- Music_App/01.js
+++ Music_App/01.js
@@ -31,21 +31,6 @@ progress.onchange = function () {
     ctrlIcon.classList.remove("fa-play");
 }
 
-// function updateTime() {
-//     let time = document.getElementById("time");
-//     time.textContent = song.currentTime + "/" + song.duration;
-// }
-
-// updateTime();
-// setInterval(updateTime, 100);
-
-// function formatTime(seconds) {
-//     const minutes = Math.floor(seconds / 60);
-//     const secs = Math.floor(seconds % 60);
-//     return `${minutes}:${secs.toString().padStart(2, '0')}`;
-// }
-
-
 const currentTimeDisplay = document.getElementById('current-time');
 const durationDisplay = document.getElementById('duration');
 
