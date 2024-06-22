adb0751aac127f1c1ab9dc9f55f6e25e79fd7986
rachit <rachitgarg1208@gmail.com>
2024-06-22 15:36:07 +0530

    Trial First Success

--- Music_App_Advanced/Trial2.js
+++ Music_App_Advanced/Trial2.js
@@ -76,3 +76,68 @@ function end() {
 function start() {
     song.currentTime = 0
 }
+
+// function morePlay(Poster, SName, Artist, songSrc) {
+//     let fx = `<div class="container">
+//         <div class="card">
+//             <nav class="nav">
+//                 <div class="circle1" onclick="remove()">
+//                     <i class="fa-solid fa-angle-left" onclick="remove()"></i>
+//                 </div>
+
+//                 <div class="circle2" onclick="Show()">
+//                     <i class="fa-solid fa-bars" onclick="Show()"></i>
+//                     <ul class="sidebar">
+//                         <li id="Lalkara">Lalkara</li>
+//                         <li>Softly</li>
+//                         <li>Mexico</li>
+//                         <li>Kina Chir</li>
+//                         <li>White Brown Black</li>
+//                         <li>Adminrin You</li>
+//                         <li>Bachke Bachke</li>
+//                         <li id="insane">Insane</li>
+//                         <li>Take it easy</li>
+
+
+//                     </ul>
+//                 </div>
+//             </nav>
+//             <img src="${Poster}" alt="Poster" class="poster">
+//             <div class="name">${SName}</div>
+//             <div class="artist">${Artist}</div>
+
+//             <audio id="song">
+//                 <source src=${songSrc} type="audio/mpeg">
+//             </audio>
+
+//             <input type="range" value="0" id="progress">
+//             <div class="time">
+//                 <div id="current-time">0:00</div>
+//                 <div>/</div>
+//                 <div id="duration">0:00</div>
+//             </div>
+
+//             <div class="controls">
+//                 <div onclick="start()"><i class="fa-solid fa-backward"></i></div>
+//                 <div onclick="playpause()"><i class="fa-solid fa-play" id="ctrlIcon"></i></div>
+//                 <div onclick="end()"><i class="fa-solid fa-forward"></i></div>
+//             </div>
+//         </div>
+//     </div>
+//     </div>`
+//     document.querySelector(".container").innerHTML = fx;
+// }
+
+let artist = document.getElementById("artist");
+let poster = document.getElementById("poster");
+let name = document.getElementById("name");
+
+function insane() {
+    let element = document.getElementById("insane");
+    song.src = "Songs/Insane.mp3";
+    artist.innerHTML = "AP Dhillon";
+    name.innerHTML = "Insane";
+    poster.src = "Poster/insane.jpg";
+    song.play();
+    ctrlIcon.classList.add("fa-pause");
+}
